6288e8533b445c28509c892066fb85d178c2443e
Madman10K <stanv.contact176@gmail.com>
2026-03-07 16:31:16 +0200

    Update

--- utils/remove-file-by-suffix.py
+++ utils/remove-file-by-suffix.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # Copyright 2015 The Shaderc Authors. All rights reserved.
 #
--- utils/update_build_version.py
+++ utils/update_build_version.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # Copyright 2016 The Shaderc Authors. All rights reserved.
 #
